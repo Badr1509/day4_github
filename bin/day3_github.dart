0caba93c9f621276ea4584178c33c3fb8063f723
BadrAlmousa <xiw92x@gmail.com>
2024-12-04 15:19:35 +0300

    day4_github

---
+++ bin/day3_github.dart
@@ -0,0 +1,13 @@
+void main() {
+  checkEvenOrOdd(4);
+  checkEvenOrOdd(7); 
+}
+void checkEvenOrOdd(int number) {
+  if (number % 2 == 0) {
+    print("Even");
+  } else {
+    print("Odd");
+  }
+}
+
+
---
+++ test/day3_github_test.dart
@@ -0,0 +1,8 @@
+import 'package:day3_github/day3_github.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
